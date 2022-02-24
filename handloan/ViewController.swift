ce23720465d30df20f0c444a3cc2a9df12ed9a28
mehulb <mehulb@live.com>
2022-02-25 01:41:16 +0530

    Project added

---
+++ handloan/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  handloan
+//
+//  Created by Mehul Bhavani on 25/02/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
